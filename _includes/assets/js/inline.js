b608a3eae6396b563e4ae2d65897062017c67c69
Hardy Ruparel <hardy.ruparel@vanmoof.com>
2022-11-28 16:48:17 +0100

    rand bg colors

--- _includes/assets/js/inline.js
+++ _includes/assets/js/inline.js
@@ -1,1 +1,8 @@
 // Add your inline JS here
+function changeBackground() {
+	var colors = ['#e8b5b5', '#e8dab5', '#d1e8b5', '#b5e8e7', '#c4b5e8', '#e8b5da', '#e8b5b5'];
+	var color = colors[Math.floor(Math.random()*colors.length)];
+   	document.body.style.background = color;
+}
+
+window.addEventListener("load",function() { changeBackground() });
